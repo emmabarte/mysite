ea3052c2105ae0806c015d496b25856cf6be0273
emmabarte <emmabarte47@gmail.com>
2024-06-10 14:49:03 +0800

    fixing minor bugs

--- djangocrudelesson/views.py
+++ djangocrudelesson/views.py
@@ -51,6 +51,7 @@ def update_gender(request, gender_id):
 
 def delete_gender(request, gender_id):
     gender = Gender.objects.get(pk=gender_id) # SELECT * FROM genders WHERE gender_id = gender_id
+    print(gender)
 
     context = {
         'gender': gender,
@@ -61,20 +62,19 @@ def delete_gender(request, gender_id):
 def destroy_gender(request, gender_id):
     Gender.objects.filter(pk=gender_id).delete() # DELETE FROM genders WHERE gender_id = gender_id
     messages.success(request, 'Gender successfully deleted.')
-
     return redirect('/genders')
 
 def index_user(request):
-    users = User.objects.select_related('gender') # SELECT * FROM users LEFT JOIN genders ON users.gender_id = genders.gender_id 
+    users = User.objects.select_related('gender') 
 
     context = {
-        'users' : users, 
+        'users': users,
     }
 
     return render(request, 'user/index.html', context)
 
 def create_user(request):
-    genders = Gender.objects.all() # SEELCT * FROM genders
+    genders = Gender.objects.all() 
 
     context = {
         'genders': genders
@@ -87,8 +87,8 @@ def store_user(request):
     middleName = request.POST.get('middle_name')
     lastName = request.POST.get('last_name')
     age = request.POST.get('age')
-    birthDate = request.POST.get('birth_date')
     genderId = request.POST.get('gender_id')
+    birthDate = request.POST.get('birth_date')
     username = request.POST.get('username')
     password = request.POST.get('password')
     confirmPassword = request.POST.get('confirm_password')
@@ -96,12 +96,60 @@ def store_user(request):
     if password == confirmPassword:
         encryptedPassword = make_password(password)
 
-        User.objects.create(first_name=firstName, middle_name=middleName, last_name=lastName, age=age, birth_date=birthDate, 
-        gender_id=genderId, username=username, password=encryptedPassword)
+        User.objects.create(first_name=firstName, middle_name=middleName, last_name=lastName, age=age, birth_date=birthDate, username=username, password=encryptedPassword, gender_id = genderId )
 
         messages.success(request, 'User successfully saved.')
 
-        return redirect('/users')
-    else:
-        messages.error(request, 'Password do not match,')
         return redirect('/user/create')
+    
+def show_user(request, user_id):
+    user = User.objects.select_related('gender').get(pk=user_id)
+
+    context = {
+        'user': user,
+    }
+
+    return render(request, 'user/show.html', context)
+
+def edit_user(request, user_id):
+    user = User.objects.select_related("gender").get(pk=user_id)
+    context = {'user': user}
+    return render(request, 'user/edit.html', context)
+
+
+def update_user(request, user_id):
+    firstName = request.POST.get('first_name')
+    middleName = request.POST.get('middle_name')
+    lastName = request.POST.get('last_name')
+    age = request.POST.get('age')
+    birthDate = request.POST.get('birth_date')
+    username = request.POST.get('username')
+    gender = request.POST.get('gender')
+    gender_id = User.objects.select_related("gender").values("gender_id").first()["gender_id"]
+    User.objects.filter(pk=user_id).update(first_name=firstName, middle_name=middleName, last_name=lastName, age=age, birth_date=birthDate, username=username)
+    Gender.objects.filter(pk=gender_id).update(gender=gender) #UPDATE genders SET gender = gender WHERE gender_id = gender_id
+    messages.success(request, 'Gender successfully updated')
+    
+    return redirect("/users")
+
+   
+def delete_user(request, user_id):
+    user = User.objects.get(pk=user_id) # SELECT * FROM users WHERE user_id = user_id
+
+    context = {
+        'user': user,
+    }
+    return render(request, "user/delete.html", context)
+
+
+def destroy_user(request, user_id):
+    user = User.objects.filter(pk=user_id).values("username")
+    if user.exists():
+        username = user.first()["username"]
+    else:
+        username = "Uknown"
+    User.objects.filter(pk=user_id).delete()
+
+    messages.success(request, f'The user named {username} has been deleted.')
+    return redirect("/users")
+
